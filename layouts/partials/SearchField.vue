8b93c92ad2a20ff33d817bfb84d4d6224d70ab29
Leandro Acquati <leandroacquati@gmail.com>
2019-07-03 18:09:37 -0300

    Search input error feedback

--- layouts/partials/SearchField.vue
+++ layouts/partials/SearchField.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <v-text-field v-model="searchInput" label="Pesquisar Personagem">
+    <v-text-field
+      v-model="searchInput"
+      label="Pesquisar Personagem"
+      @keyup.enter.native="getCharacters"
+    >
       <template slot="append">
         <v-icon v-if="hasText" @click="clearButton">clear</v-icon>
       </template>
@@ -8,19 +12,32 @@
         <v-icon @click="getCharacters">search</v-icon>
       </template>
     </v-text-field>
-    <span>{{ characters }}</span>
+    <p>{{ marvelError }}</p>
+    <ul v-for="character in characters" :key="character.id">
+      <li>
+        {{ character.thumbnail.path }}
+      </li>
+      <li>
+        {{ character.name }}
+      </li>
+      <li v-if="character.description != ''">
+        {{ character.description }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import { private_key, public_key } from '~/marvel'
 import md5 from 'blueimp-md5'
+import _ from 'lodash'
 
 export default {
   data() {
     return {
       searchInput: '',
-      characters: []
+      characters: [],
+      marvelError: 'Pesquise uma personagem da marvel.'
     }
   },
   computed: {
@@ -28,11 +45,25 @@ export default {
       return this.searchInput.length
     }
   },
+  watch: {
+    searchInput() {
+      if (this.searchInput != '') {
+        this.marvelError = 'Esperando você terminar de digitar...'
+      } else {
+        this.marvelError = 'Pesquise uma personagem da marvel.'
+      }
+      this.debouncedGetCharacters()
+    }
+  },
+  created() {
+    this.debouncedGetCharacters = _.debounce(this.getCharacters, 750)
+  },
   methods: {
     clearButton() {
       this.searchInput = ''
     },
     getCharacters() {
+      if (this.searchInput == '') return
       this.$axios
         .$get(
           'http://gateway.marvel.com/v1/public/characters?nameStartsWith=' +
@@ -45,11 +76,18 @@ export default {
             md5(this.$moment().format() + private_key + public_key)
         )
         .then(result => {
-          console.log(result.data.results)
-          this.characters = result.data.results
+          if (result.data.results.length == 0) {
+            this.characters = []
+            this.marvelError = 'Nenhuma personagem encontrada com essas letras.'
+          } else {
+            this.characters = result.data.results
+            result.data.results.length == 1
+              ? (this.marvelError = 'Personagem encontrada.')
+              : (this.marvelError = 'Personagens encontradas.')
+          }
         })
         .catch(error => {
-          console.log(error)
+          this.marvelError = error
         })
     }
   }
